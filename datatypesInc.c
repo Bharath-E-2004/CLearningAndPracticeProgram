bf43c597ed57bba767138225dbd483f866e9856d
Bharath-E-2004 <mr.bharath.e@gmail.com>
2025-12-05 23:46:59 +0530

    Create datatypesInc.c to showcase C data types
    Added a program demonstrating various data types in C.

---
+++ datatypesInc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a; // 4 bits
+    float b; // 4 bits
+    double d; // 8 bits
+    char c;   // 1 bits
+    long int l; // 8 bits
+    signed int s; // 4 bits positive integer
+    unsigned int i; // 4 bits negative and positive number
+    long double ld;  // 10 to 16 bits
+    short int s; // 2 bits
+    
+    //datatypes in c
+}
