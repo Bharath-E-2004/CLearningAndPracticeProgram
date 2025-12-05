3c852ba3a322b7c936eb5a8c6ac6d2a7e20c8d5e
Bharath-E-2004 <mr.bharath.e@gmail.com>
2025-12-05 23:49:33 +0530

    FirstProgram.c

---
+++ HelloWorld.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    printf("Hello World\n");
+
+    printf("%d\rABC\n",12345); // Carriage return Example for overriding in the console
+    return 0;
+}
+/*
+ this was used for mutliple file compilation
+    gcc HelloWorld.c -o HelloWorld.exe
+*/
+
+//hi
